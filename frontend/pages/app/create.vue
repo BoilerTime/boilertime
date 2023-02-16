3e75a15c2e36cedb37a9e1a920435606aba8744e
Justin Lai <justinlai2001@yahoo.com>
2023-02-15 19:19:11 -0500

    Remove Sample Commits

--- frontend/pages/app/create.vue
+++ frontend/pages/app/create.vue
@@ -2,18 +2,5 @@
   <h1 class="text-3xl font-bold underline">
     Onboarding goes here - authenticated page
   </h1>
-
-  <body>
-    <div class="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 sm:py-12">
-      <div class="px-4 text-center sm:px-0">
-        <h1 class="text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-          <span class="block xl:inline">Welcome to</span>
-          <span class="block text-indigo-600 xl:inline">Your App</span>
-        </h1>
-        <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
-        </p>
-      </div>
-    </div>
-  </body>
+  
 </template>
