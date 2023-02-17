79108c56a7ca782cd1c176e65bebc770288f3326
Joshua Yang <joshuajy03@gmail.com>
2023-02-17 15:06:18 -0500

    add classes to collection

--- backend/components/schedule/schedule.js
+++ backend/components/schedule/schedule.js
@@ -10,14 +10,16 @@ const db = getFirestore()
 const schedules = db.collection('user_schedules')
 
 module.exports = {
-    authenticate
+    addClasses
 }
 
-async function authenticate({ user_id, optional_classes, personal_preferences, required_classes }) {
+async function addClasses({ user_id, optional_classes, personal_preferences, required_classes }) {
     const schedule = await users.where('user_id', '==', user_id).get();
     profile.forEach(doc => {
-      const optional_classes = doc.collection(optional_classes);
-      const required_classes = doc.collection(required_classes);
-      const preferences = doc.collection(preferences);
+       doc.collection('sprint_2023').add({
+          required_classes: required_classes,
+          optional_classes: optional_classes,
+          personal_preferences: personal_preferences
+      });
     });
 }
