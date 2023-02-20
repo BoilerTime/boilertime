0fd8f3738343f6227fba8b32451ed8078d0c247e
Henry Mayer <hsmayer7@gamil.com>
2023-02-20 16:05:41 -0500

    Added write to file for a list of all courses.

--- backend/components/datasources/purdueios.js
+++ backend/components/datasources/purdueios.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+var fs = require('fs');
 
 const purdueios = async function() {
 	console.log("Called");
@@ -13,11 +14,16 @@ const purdueios = async function() {
 			subjects.push(res.Abbreviation);
 	});
 	console.log(subjects.length);
+	//for(var i=0; i<1; i++) {
 	for(var i=0; i<subjects.length; i++) {
-			courses.push(await getCoursesBySubject(subjects[i]));
+			courses.push({"subject": subjects[i], "courses": await getCoursesBySubject(subjects[i])});
 	}
 
 	console.log(courses);
+	fs.writeFile('courses.json', JSON.stringify(courses), function (err) {
+		if (err) throw err;
+		console.log('Saved!');
+	});
 	//return body;
 }
 
@@ -26,7 +32,7 @@ const getCoursesBySubject = async function(subject) {
 	let courseList = [];
 	let courses = await fetch('https://api.purdue.io/odata/Courses?$filter=(Classes/any(c:%20c/Term/Code%20eq%20%27202320%27))%20and%20Subject/Abbreviation%20eq%20%27' + subject + '%27');
 	courses = await courses.json();
-	return courses;
+	return courses.value;
 }
 
 
