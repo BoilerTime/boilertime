bc9404bcaefc6e02f374ef86c7092a9c1eb211bc
Justin Lai <justinlai2001@yahoo.com>
2023-02-22 16:37:07 -0500

    Updated window contents and provided json functionality

--- frontend/pages/app/create.vue
+++ frontend/pages/app/create.vue
@@ -4,57 +4,97 @@
     or optional.-->
 
 <template>
-  <body class = "h-screen bg-gray-200">
-    <div class = "mx-auto my-24 max-w-screen-xl bg-white border-
-                 rounded-lg shadow-lg sm:p-8 md:p-8">
-
+  <!--Begin entire page-->
+  <body class="h-screen bg-gray-200">
+    <!--Begin window-->
+    <div
+      class="grid grid-flow-row mx-auto my-24 w-4/5 bg-white rounded-lg shadow-lg p-8"
+    >
       <!--Intro-->
-      <h1 class = "font-bold text-left text-2xl" >
-        Hi! Let's help you get started with your new schedule.
+      <h1 class="font-extrabold text-left text-2xl">
+        Hi 👋 Let's help you get started with your new schedule!
       </h1>
-      
-      <h1 class = "font-bold text-left">
-        To do so, search for your class in the bar below and mark it as required or optional.
-        Please note that you are limited to 10 classes total.
+
+      <h1 class="font-light text-left">
+        To do so, search for your class in the bar below and mark it as required
+        or optional.
       </h1>
-      <br/>
+      <br />
 
-      <!--User interaction group, fix positioning on the screen, fill lists as boxes-->
-      <div>
+      <!--List display-->
+      <div class="grid grid-cols-2 gap-4">
+        <div>
+          <h1 class="font-extrabold text-left text-2xl">Required Classes</h1>
+          <div class="mx-auto my-auto w-auto bg-gray-200 rounded-lg p-4">
+            <ol class="list-decimal list-inside">
+              <!-- Loop through the array and display each item as a list item -->
+              <li
+                class="mb-2 font-bold"
+                v-for="(item, index) in required_classes"
+                :key="index"
+              >
+                {{ item }}
+              </li>
+            </ol>
+          </div>
+        </div>
 
-        <input class = "border-2 border-gray-300 bg-white h-10 px-5
-                        rounded-lg text-sm focus:outline-none"
-               type = "text" v-model = "class_input" placeholder = "Enter class...">
-
-        <button class = "bg-blue-500 hover:bg-blue-700 text-white
-                         font-bold py-2 px-4 rounded"
-                type = "button" @click = "add_class_required">
-          Required
-        </button>
-
-        <button class = "bg-blue-500 hover:bg-blue-700 text-white
-                         font-bold py-2 px-4 rounded"
-                type = "button" @click = "add_class_optional">
-          Optional
-        </button>
-
-        <button class = "bg-blue-500 hover:bg-blue-700 text-white
-                         font-bold py-2 px-4 rounded"
-                type = "button" @click = "submit">
-          Done
-        </button>
-
-        <ul class="list-disc list-inside">
-          <!-- Loop through the array and display each item as a list item -->
-          <li class="mb-2 text-red-400" v-for="(item, index) in optional_classes" :key="index">{{ item }}</li>
-        </ul>
-
-        <ul class="list-disc list-inside">
-          <!-- Loop through the array and display each item as a list item -->
-          <li class="mb-2 text-cyan-400" v-for="(item, index) in required_classes" :key="index">{{ item }}</li>
-        </ul>
-
+        <div>
+          <h1 class="font-extrabold text-left text-2xl">Optional Classes</h1>
+          <div class="mx-auto my-auto w-auto bg-gray-200 rounded-lg p-4">
+            <ol class="list-decimal list-inside">
+              <!-- Loop through the array and display each item as a list item -->
+              <li
+                class="mb-2 font-bold"
+                v-for="(item, index) in optional_classes"
+                :key="index"
+              >
+                {{ item }}
+              </li>
+            </ol>
+          </div>
+        </div>
       </div>
+
+      <br />
+
+      <!--User interaction group-->
+      <div class="grid gap-4 grid-cols-2">
+        <input
+          class="w-5/6 justify-self-center border-2 border-gray-300 bg-white h-10 px-5 rounded-lg text-sm focus:outline-none"
+          type="text"
+          v-model="class_input"
+          placeholder="Enter class..."
+        />
+
+        <div class="flex gap-4 justify-self-center">
+          <button
+            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            type="button"
+            @click="add_class_required"
+          >
+            Required
+          </button>
+
+          <button
+            class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            type="button"
+            @click="add_class_optional"
+          >
+            Optional
+          </button>
+        </div>
+      </div>
+
+      <br />
+      <!--Submit Button-->
+      <button
+        class="w-1/8 justify-self-end bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        type="button"
+        @click="submit"
+      >
+        Done
+      </button>
     </div>
   </body>
 </template>
@@ -62,26 +102,29 @@
 <!-- Begin scripting section -->
 
 <script>
-
 import axios from "axios";
 
 export default {
-
   class_input: "",
-  url: "backend/components/schedule/schedule.js",
 
   data() {
-
     return {
-      user_id: "dummy_user_id",
+      user_id: "",
       optional_classes: [],
       personal_preferences: [],
       required_classes: [],
     };
-
   },
 
   methods: {
+    return_data() {
+      return {
+        user_id: "dummy_user_id",
+        optional_classes: this.optional_classes,
+        personal_preferences: [],
+        required_classes: this.required_classes,
+      };
+    },
 
     add_class_required() {
       if (this.class_input !== "") {
@@ -98,13 +141,14 @@ export default {
     },
 
     submit() {
-      axios.post(url, data())
-      .then(res => console.log(res))
-      .catch(err => console.log(err))
-      navigateTo("/app/loading");
+      axios
+        .post("http://  localhost:3001/api/createschedule", this.return_data())
+        .then((res) => {
+          console.log(res);
+          navigateTo("/app/loading");
+        })
+        .catch((err) => console.log(err));
     },
-
   },
-
 };
 </script>
