fb8c589b5e5a9f3a3fc589aee2cefe09d80948d5
NicholasPowers <35582171+Stale-Bread@users.noreply.github.com>
2023-02-26 14:47:34 -0500

    Removed an unused function

--- frontend/pages/auth/register.vue
+++ frontend/pages/auth/register.vue
@@ -156,12 +156,4 @@
       alert("Passwords do not match")
     }
   }
-
-  async function hash(password) {
-    const utf8 = new TextEncoder().encode(password);
-    const hashBuffer = await crypto.subtle.digest('SHA-256', utf8);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map((bytes) => bytes.toString(16).padStart(2, '0')).join('');
-    return hashHex;
-  }
 </script>
