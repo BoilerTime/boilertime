1c35085e8744c49ccb88c31274b10914c1a7c2b4
Onyx Mayer <hsmayer7@gamil.com>
2023-02-26 18:01:54 -0500

    Created elementary method for chromosome and crossover.

---
+++ optimizer/Chromosome.java
@@ -0,0 +1,68 @@
+package optimizer;
+import java.util.Random;
+
+public class Chromosome {
+
+    private String data;
+    private Random r;
+
+    
+    public Chromosome(String chromosome) {
+        this.data = chromosome;
+        r = new Random();
+    }
+
+    /**
+     * This function generates a new binary chromosome based on the contents of the chromsome in question and another one passed to it 
+     * @param c2 The second chromosome that is to be crossed over
+     * @return A new chromosome that represents the results of the cross 
+     */
+    public Chromosome[] crossOver(Chromosome c2) {
+        String cr2 = c2.getChromosome();
+        int crossLength = Math.min(data.length(), cr2.length()); //We can only crossover based on the length of the shortest chromsome. Any extraneous portion will go un-evolved
+        int startPosition = randInRange(0, crossLength-1); //Only start from the position one before the end of the chromosome
+        int endPosition = randInRange(startPosition, crossLength); //We need a positive range to cut that starts and start position and ends at or prior to the length of the chromsome
+        //String c1Segment = data.substring(startPosition, endPosition);
+        Chromosome[] results = new Chromosome[2];
+        char crh1[] = getCharChromsome(data);
+        char crh2[] = getCharChromsome(cr2);
+        for(int i = startPosition; i < endPosition; i++) {
+            char temp = crh1[i];
+            crh1[i] = crh2[i];
+            crh2[i] = temp;
+        }
+        results[0] = new Chromosome(new String(crh1));
+        results[1] = new Chromosome(new String(crh2));
+        return results;
+    }
+
+    /**
+     * A method to get a random number that lies within a range [max, min]
+     * @param min {int} The min value to find a random value for 
+     * @param max {int} The max value to find a random value for 
+     * @return {int} A psuedo-random value that lies within the range [min, max]
+     */
+    private int randInRange(int min, int max) {
+		return r.nextInt((max - min) + 1) + min;
+	}
+
+    /**
+     * A method to get the String contents of the current chromosome 
+     * @return {String} The String contents of the chromosome 
+     */
+    public String getChromosome() {
+        return this.data;
+    }
+
+    /**
+     * A method to get the entire char aray the represents the chromsome
+     * @return The char array
+     */
+    private char[] getCharChromsome(String c) {
+        char[] chromsome = new char[c.length()];
+        for(int i = 0; i < chromsome.length; i++) {
+            chromsome[i] = c.charAt(i);
+        }
+        return chromsome;
+    }
+}
---
+++ optimizer/optimizer.java
@@ -0,0 +1,8 @@
+public class Optimizer {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        Chromosome[] chromosomes = new Chromosome[5];
+    }
+
+    
+}
