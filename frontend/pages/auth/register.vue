fd512eef66bd3824112129358dec55370d8257e5
NicholasPowers <35582171+Stale-Bread@users.noreply.github.com>
2023-02-26 15:47:40 -0500

    Adjusted hashing to better work with making sure passwords match

--- frontend/pages/auth/register.vue
+++ frontend/pages/auth/register.vue
@@ -10,42 +10,48 @@
       <form @submit.prevent="() => signup()">
         <!--Email text & input box-->
         <label for="email" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">Email</label>
-        <input type="email" id="email" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-        focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500"
-          placeholder="username@purdue.edu" v-model="email" required>
+        <input type="email" id="email" aria-describedby="helper-text-explanation"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+              focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500" placeholder="username@purdue.edu"
+          v-model="email" required>
         <div class="gap-8 columns-2">
           <!--First Name text & input box-->
-          <label for="firstname" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">First Name</label>
-          <input type="firstname" id="firstname" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-          focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500"
-            v-model="firstname" required>
-          
+          <label for="firstname" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">First
+            Name</label>
+          <input type="firstname" id="firstname" aria-describedby="helper-text-explanation"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500" v-model="firstname" required>
+
           <!--Last Name text & input box-->
-          <label for="lastname" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">Last Name</label>
-          <input type="lastname" id="lastname" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-          focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500"
-            v-model="lastname" required>
+          <label for="lastname" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">Last
+            Name</label>
+          <input type="lastname" id="lastname" aria-describedby="helper-text-explanation"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500" v-model="lastname" required>
         </div>
 
         <div class="gap-8 columns-2">
           <!--Password text & input box-->
           <label for="password" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">Password</label>
-          <input type="password" id="password" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-          focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500"
-          v-model="password" required>
+          <input type="password" id="password" aria-describedby="helper-text-explanation"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500" v-model="password" required>
 
           <!--Confirm Password text & input box-->
-          <label for="confpassword" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">Confirm Password</label>
-          <input type="password" id="confpassword" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-          focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500"
-            v-model="confpassword" required>
+          <label for="confpassword" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">Confirm
+            Password</label>
+          <input type="password" id="confpassword" aria-describedby="helper-text-explanation"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500" v-model="confpassword" required>
         </div>
 
         <div class="gap-8 columns-2">
           <!--Graduation month-->
-          <label for="gradmonth" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">Graduation Month</label>
-          <select id="gradmonth" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
-            p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          <label for="gradmonth" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">Graduation
+            Month</label>
+          <select id="gradmonth"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
+                  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             v-model="gradmonth" required>
             <option value="january">January</option>
             <option value="february">February</option>
@@ -61,9 +67,11 @@
             <option value="december">December</option>
           </select>
           <!--Graduation year-->
-          <label for="gradyear" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">Graduation Year</label>
-          <select id="gradyear" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
-            p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          <label for="gradyear" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">Graduation
+            Year</label>
+          <select id="gradyear"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
+                  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             v-model="gradyear" required>
             <option value="2023">2023</option>
             <option value="2024">2024</option>
@@ -87,23 +95,29 @@
         </div>
 
         <!--Grad/Undergrad checkbox-->
-        <label for="gradstatus" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">Student Status</label>
-        <ul class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+        <label for="gradstatus" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">Student
+          Status</label>
+        <ul
+          class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
           <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
             <div class="flex items-center pl-3">
-              <input id="isGradStudent" type="radio" value="false" name="isGradStudent" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600
-                dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" v-model="isGradStudent" required>
-              <label for="isGradStudent" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Undergraduate</label>
+              <input id="isGradStudent" type="radio" value="false" name="isGradStudent"
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600
+                      dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" v-model="isGradStudent" required>
+              <label for="isGradStudent"
+                class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Undergraduate</label>
             </div>
           </li>
           <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
             <div class="flex items-center pl-3">
-              <input id="isGradStudent" type="radio" value="true" name="isGradStudent" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600
-                dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" v-model="isGradStudent" required>
-              <label for="gradstatus" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Graduate</label>
+              <input id="isGradStudent" type="radio" value="true" name="isGradStudent"
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600
+                      dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" v-model="isGradStudent" required>
+              <label for="gradstatus"
+                class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Graduate</label>
             </div>
           </li>
-        </ul>        
+        </ul>
 
         <!--Attempts to register-->
         <div class="container pt-5 px-5 min-w-full flex flex-col items-center">
@@ -115,57 +129,60 @@
     </div>
   </div>
 </template>
-    
+        
 <script setup>
-  import { ref } from 'vue'
-  import axios from 'axios'
-  import sha256 from 'js-sha256'
+import { ref } from 'vue'
+import axios from 'axios'
+import sha256 from 'js-sha256'
 
-  const email = ref('')
-  const firstname = ref('')
-  const lastname = ref('')
-  let password = ref('')
-  const confpassword = ref('')
-  const gradmonth = ref('')
-  const gradyear = ref('')
-  const isGradStudent = ref('')
+const email = ref('')
+const firstname = ref('')
+const lastname = ref('')
+let password = ref('')
+const confpassword = ref('')
+const gradmonth = ref('')
+const gradyear = ref('')
+const isGradStudent = ref('')
 
-  /**
-   * This function will take inputted data and create an account.
-   * It will also check to see if the passwords match.
-   */
-  async function signup() {
-    if (password.value===confpassword.value) {
-      var hash = sha256.create();
-      hash.update(password.value);
-      password.value = hash.hex();
-      //console.log(password.value)
-      await axios.post('http://localhost:3001/api/createuser', {
-        firstname: firstname.value,
-        lastname: lastname.value,
-        email: email.value,
-        password: password.value,
-        gradmonth: gradmonth,
-        gradyear: gradyear.value,
-        isGradStudent: isGradStudent.value
-      })
+/**
+ * This function will take inputted data and create an account.
+ * It will also check to see if the passwords match.
+ */
+async function signup() {
+  var hash = sha256.create();
+  hash.update(password.value);
+  password.value = hash.hex();
+
+  hash.update(confpassword.value);
+  confpassword.value = hash.hex();
+  if (password.value === confpassword.value) {
+    //console.log(password.value)
+    await axios.post('http://localhost:3001/api/createuser', {
+      firstname: firstname.value,
+      lastname: lastname.value,
+      email: email.value,
+      password: password.value,
+      gradmonth: gradmonth,
+      gradyear: gradyear.value,
+      isGradStudent: isGradStudent.value
+    })
       .catch(function (error) {
         console.error();
       });
-    } else {
-      alert("Passwords do not match")
-    }
+  } else {
+    alert("Passwords do not match")
   }
-  async function sendemail() {
-    await axios.post('http://localhost:3001/api/createuser', {
-      email: email.value
-    })
-    .then (function () {
+}
+async function sendemail() {
+  await axios.post('http://localhost:3001/api/createuser', {
+    email: email.value
+  })
+    .then(function () {
       alert("A verification email has been sent. Please verify before continuing.")
       navigateTo("/auth/verifyemail")
     })
     .catch(function (error) {
       console.error(error)
     });
-  }
+}
 </script>
