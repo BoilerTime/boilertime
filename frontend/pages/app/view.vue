a91690c74763eca5dee57b5963bcee34adc34889
Zachary Will Sy <alpinemail@zsy.sh>
2023-02-26 20:02:37 -0500

    feat: retrieve schedule data to front end

--- frontend/pages/app/view.vue
+++ frontend/pages/app/view.vue
@@ -3,4 +3,25 @@
     View optimized final schedule here - list and calendar view - auth protected
   </h1>
 </template>
-  
+
+<script setup>
+import axios from 'axios'
+
+const data = []
+
+async function getOptimized() {
+  await axios.get("http://localhost:3001/api/optimizedschedule")
+    .then(function (res) {
+      data = res.data.schedule
+    })
+    .catch(function (err) {
+      alert("Failed to retrieve data. Please refresh page")
+    })
+}
+
+onMounted(() => {
+  getOptimized()
+  console.log(data)
+})
+
+</script>  
