8971f42da5d464869bd9de0364c2dc9319d588e4
NicholasPowers <35582171+Stale-Bread@users.noreply.github.com>
2023-02-27 18:32:52 -0500

    Fixed conflicts in user story 2

--- frontend/pages/auth/register.vue
+++ frontend/pages/auth/register.vue
@@ -12,7 +12,7 @@
         <label for="email" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">Email</label>
         <input type="email" id="email" aria-describedby="helper-text-explanation"
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-              focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500" placeholder="username@purdue.edu"
+                focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500" placeholder="username@purdue.edu"
           v-model="email" required>
         <div class="gap-8 columns-2">
           <!--First Name text & input box-->
@@ -20,14 +20,14 @@
             Name</label>
           <input type="firstname" id="firstname" aria-describedby="helper-text-explanation"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-                focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500" v-model="firstname" required>
+                  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500" v-model="firstname" required>
 
           <!--Last Name text & input box-->
           <label for="lastname" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">Last
             Name</label>
           <input type="lastname" id="lastname" aria-describedby="helper-text-explanation"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-                focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500" v-model="lastname" required>
+                  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500" v-model="lastname" required>
         </div>
 
         <div class="gap-8 columns-2">
@@ -35,14 +35,14 @@
           <label for="password" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">Password</label>
           <input type="password" id="password" aria-describedby="helper-text-explanation"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-                focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500" v-model="password" required>
+                  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500" v-model="password" required>
 
           <!--Confirm Password text & input box-->
           <label for="confpassword" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">Confirm
             Password</label>
           <input type="password" id="confpassword" aria-describedby="helper-text-explanation"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-                focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500" v-model="confpassword" required>
+                  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500" v-model="confpassword" required>
         </div>
 
         <div class="gap-8 columns-2">
@@ -51,7 +51,7 @@
             Month</label>
           <select id="gradmonth"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
-                  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             v-model="gradmonth" required>
             <option value="january">January</option>
             <option value="february">February</option>
@@ -71,7 +71,7 @@
             Year</label>
           <select id="gradyear"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
-                  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             v-model="gradyear" required>
             <option value="2023">2023</option>
             <option value="2024">2024</option>
@@ -102,18 +102,18 @@
           class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
           <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
             <div class="flex items-center pl-3">
-              <input id="isGradStudent" type="radio" value="false" name="isGradStudent"
+              <input id="isGraduateStudent" type="radio" value="false" name="isGraduateStudent"
                 class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600
-                      dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" v-model="isGradStudent" required>
-              <label for="isGradStudent"
+                        dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" v-model="isGraduateStudent" required>
+              <label for="isGraduateStudent"
                 class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Undergraduate</label>
             </div>
           </li>
           <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
             <div class="flex items-center pl-3">
-              <input id="isGradStudent" type="radio" value="true" name="isGradStudent"
+              <input id="isGraduateStudent" type="radio" value="true" name="isGraduateStudent"
                 class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600
-                      dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" v-model="isGradStudent" required>
+                        dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" v-model="isGraduateStudent" required>
               <label for="gradstatus"
                 class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Graduate</label>
             </div>
@@ -129,22 +129,18 @@
       </form>
     </div>
   </div>
-
 </template>
         
 <script setup>
 import { ref } from 'vue'
 import axios from 'axios'
 import sha256 from 'js-sha256'
-
-
 const email = ref('')
 const firstname = ref('')
 const lastname = ref('')
 const password = ref('')
 const confpassword = ref('')
-
-
+const isGraduateStudent = ref('')
 /**
  * This function will take inputted data and create an account.
  * It will also check to see if the passwords match.
@@ -154,8 +150,7 @@ async function signup() {
   var newconfpassword = sha256(confpassword.value);
   //console.log(newpassword);
   //console.log(newconfpassword);
-  if (newpassword===newconfpassword) {
-
+  if (newpassword === newconfpassword) {
     await axios.post('http://localhost:3001/api/createuser', {
       firstname: firstname.value,
       lastname: lastname.value,
@@ -165,9 +160,11 @@ async function signup() {
       gradyear: gradyear.value,
       isGraduateStudent: isGraduateStudent.value
     })
+      .then(function() {
+        navigateTo("/auth/verifyaccount");
+      })
       .catch(function (error) {
         console.error();
-
       });
   } else {
     alert("Passwords do not match")
---
+++ frontend/pages/auth/verifyaccount.vue
@@ -0,0 +1,33 @@
+<!--The purpose of verifyemail.vue is to show the user a page requesting them
+    to verify their email after registering for an account.-->
+<template>
+    <div class="h-screen bg-gradient-to-b from-gray-100 to-gray-300">
+        <br />
+        <div class="mx-auto my-10 5px max-w-sm p-6 bg-white border rounded-lg shadow sm:p-8 md:p-8 dark:bg-white">
+            <!--Verified Email text-->
+            <h1 class="pb-4 text-center text-2x1 font-bold">Thank you for verifying your email. Automatically
+                redirecting you to the Login Page.</h1>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+
+const user_id = route.query.user_id
+
+/**
+ * A function that will make sure the user's userid matches with the emailed one.
+ */
+async function verifyaccount() {
+    await axios.post('http://localhost:3001/api/verifyaccount', {
+        user_id: user_id
+    })
+    .then(function () {
+        navigateTo("/auth/login")
+    })
+    .catch(function (error) {
+        console.error(error)
+    })
+}
+</script>
