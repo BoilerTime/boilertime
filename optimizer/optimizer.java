93aed90d9e2edddd799a800ae4e40328e7b64c00
Onyx Mayer <hsmayer7@gamil.com>
2023-02-26 20:02:19 -0500

    Added testing in optimizer.java and a mutation method

--- optimizer/optimizer.java
+++ optimizer/optimizer.java
@@ -1,8 +1,15 @@
+package optimizer;
+import java.util.Random;
+
 public class Optimizer {
     public static void main(String[] args) {
-        System.out.println("Hello!");
-        Chromosome[] chromosomes = new Chromosome[5];
+        Chromosome c1 = new Chromosome("100000011011010100101");
+        Chromosome c2 = new Chromosome("011101101010011111110");
+        Chromosome[] population = new Chromosome[5];
+        population = c1.crossOver(c2);
+        System.out.println(population[0].getChromosome());
+        population[1] = c1.mutate();
+        System.out.println(population[1].getChromosome());
     }
-
     
 }
