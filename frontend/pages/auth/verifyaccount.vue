272ef312f7326f91afaaef869a6384d000cd8a10
NicholasPowers <35582171+Stale-Bread@users.noreply.github.com>
2023-02-28 19:51:42 -0500

    Updated verifyaccount

--- frontend/pages/auth/verifyaccount.vue
+++ frontend/pages/auth/verifyaccount.vue
@@ -5,28 +5,30 @@
         <br />
         <div class="mx-auto my-10 5px max-w-sm p-6 bg-white border rounded-lg shadow sm:p-8 md:p-8 dark:bg-white">
             <!--Verified Email text-->
-            <h1 class="pb-4 text-center text-2x1 font-bold">Thank you for verifying your account. Redirecting you to the Login Page.</h1>
+            <h1 class="pb-4 text-center text-2x1 font-bold">Verifying your account right now. Please wait.</h1>
         </div>
     </div>
 </template>
 
 <script setup>
+import { ref, onBeforeMount } from 'vue'
 import axios from 'axios'
 
+const route = useRoute()
 const user_id = route.query.user_id
 
 /**
  * A function that will make sure the user's userid matches with the emailed one.
  */
-window.onload = function() {
+onBeforeMount(() => {
     axios.post('http://localhost:3001/api/verifyaccount', {
         user_id: user_id
     })
-    .then(function () {
-        navigateTo("/auth/login")
-    })
-    .catch(function (error) {
-        console.error(error)
-    })
-}
+        .then(function () {
+            navigateTo("/auth/login")
+        })
+        .catch(function (error) {
+            console.error(error)
+        })
+})
 </script>
