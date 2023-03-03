a670d508db8cf91606cdafa305f162280dad07f3
NicholasPowers <35582171+Stale-Bread@users.noreply.github.com>
2023-03-02 22:25:35 -0500

    Added CAPTCHA

--- frontend/pages/rate/create.vue
+++ frontend/pages/rate/create.vue
@@ -1,5 +1,5 @@
 <!--This page is designed to allow users to create a rating of a classroom, TA, or class.
-    For the star component, it uses code found from https://tailwind-elements.com/docs/standard/components/rating/-->
+    For the captcha, this code was used https://vuejsexamples.com/build-simple-recaptcha-for-vuejs-without-server-need/-->
 <template>
     <div class="h-screen bg-gradient-to-b from-gray-100 to-gray-300">
         <br />
@@ -31,7 +31,7 @@
             <div v-if="reviewtype === 'course' && reviewselection !== 'null'">
                 <!--Prerequisite Requirement Rating aka 'rating1'-->
                 <label for="rating1" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">How strict are the prerequisite requirements?</label>
-                <center><span class="text-sm">2.5</span></center>
+                <span class="text-sm items-center">2.5</span>
                 <input id="rating1" type="range" min="0" max="5" step="0.5" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" oninput="this.previousElementSibling.innerText=this.value" v-model="rating1">
                 <div class="-mt-2 flex w-fill justify-between">
                     <span class="text-sm text-black">0</span>
@@ -39,7 +39,7 @@
                 </div>
                 <!--Pace of Materials Rating aka 'rating2'-->
                 <label for="rating2" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">How is the pace of materials covered?</label>
-                <center><span class="text-sm">2.5</span></center>
+                <span class="text-sm items-center">2.5</span>
                 <input id="rating2" type="range" min="0" max="5" step="0.5" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" oninput="this.previousElementSibling.innerText=this.value" v-model="rating2">
                 <div class="-mt-2 flex w-fill justify-between">
                     <span class="text-sm text-black">0</span>
@@ -47,7 +47,7 @@
                 </div>
                 <!--Depth of Material Rating aka 'rating3'-->
                 <label for="rating3" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">How in-depth is the material?</label>
-                <center><span class="text-sm">2.5</span></center>
+                <span class="text-sm items-center">2.5</span>
                 <input id="rating3" type="range" min="0" max="5" step="0.5" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" oninput="this.previousElementSibling.innerText=this.value" v-model="rating3">
                 <div class="-mt-2 flex w-fill justify-between">
                     <span class="text-sm text-black">0</span>
@@ -58,7 +58,7 @@
             <div v-else-if="reviewtype === 'classroom'">
                 <!--Convenience of Access Rating aka 'rating1'-->
                 <label for="rating1" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">How convenient is it to access this classroom?</label>
-                <center><span class="text-sm">2.5</span></center>
+                <span class="text-sm items-center">2.5</span>
                 <input id="rating1" type="range" min="0" max="5" step="0.5" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" oninput="this.previousElementSibling.innerText=this.value" v-model="rating1">
                 <div class="-mt-2 flex w-fill justify-between">
                     <span class="text-sm text-black">0</span>
@@ -66,7 +66,7 @@
                 </div>
                 <!--Quality of Seating Rating aka 'rating2'-->
                 <label for="rating2" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">What is the quality of seating?</label>
-                <center><span class="text-sm">2.5</span></center>
+                <span class="text-sm items-center">2.5</span>
                 <input id="rating2" type="range" min="0" max="5" step="0.5" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" oninput="this.previousElementSibling.innerText=this.value" v-model="rating2">
                 <div class="-mt-2 flex w-fill justify-between">
                     <span class="text-sm text-black">0</span>
@@ -74,7 +74,7 @@
                 </div>
                 <!--Availability of Technology Rating aka 'rating3'-->
                 <label for="rating3" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">What is the availability of technology?</label>
-                <center><span class="text-sm">2.5</span></center>
+                <span class="text-sm items-center">2.5</span>
                 <input id="rating3" type="range" min="0" max="5" step="0.5" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" oninput="this.previousElementSibling.innerText=this.value" v-model="rating3">
                 <div class="-mt-2 flex w-fill justify-between">
                     <span class="text-sm text-black">0</span>
@@ -85,7 +85,7 @@
             <div v-else-if="reviewtype === 'ta'">
                 <!--Helpfulness of Answering Questions Rating aka 'rating1'-->
                 <label for="rating1" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">How helpful are they when answering questions?</label>
-                <center><span class="text-sm">2.5</span></center>
+                <span class="text-sm items-center">2.5</span>
                 <input id="rating1" type="range" min="0" max="5" step="0.5" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" oninput="this.previousElementSibling.innerText=this.value" v-model="rating1">
                 <div class="-mt-2 flex w-fill justify-between">
                     <span class="text-sm text-black">0</span>
@@ -93,7 +93,7 @@
                 </div>
                 <!--Responsiveness Rating aka 'rating2'-->
                 <label for="rating2" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">How responsive are they?</label>
-                <center><span class="text-sm">2.5</span></center>
+                <span class="text-sm items-center">2.5</span>
                 <input id="rating2" type="range" min="0" max="5" step="0.5" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" oninput="this.previousElementSibling.innerText=this.value" v-model="rating2">
                 <div class="-mt-2 flex w-fill justify-between">
                     <span class="text-sm text-black">0</span>
@@ -101,7 +101,7 @@
                 </div>
                 <!--Fairness of Grading Rating aka 'rating3'-->
                 <label for="rating3" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">How fair is their grading?</label>
-                <center><span class="text-sm">2.5</span></center>
+                <span class="text-sm items-center">2.5</span>
                 <input id="rating3" type="range" min="0" max="5" step="0.5" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" oninput="this.previousElementSibling.innerText=this.value" v-model="rating3">
                 <div class="-mt-2 flex w-fill justify-between">
                     <span class="text-sm text-black">0</span>
@@ -109,9 +109,15 @@
                 </div>
             </div>
 
+            <!--CAPTCHA title and insert-->
+            <label for="enterval" class="pt-5 block mb-2 text-sm font-medium text-gray-900 dark:text-black">CAPTCHA: {{ randval1 }} + {{ randval2 }} =</label>
+            <input type="enterval" id="enterval" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:placeholder-black dark:text-black dark:focus:ring-blue-500"
+                  v-model="enterval" required>
+
             <!--Attempts to send review-->
-            <div class="container pt-5 px-5 min-w-full flex flex-col items-center">
-                <button type="createreview" class="bg-black hover:bg-gray-800 text-white font-bold py-2 px-10 rounded">
+            <div class="container pt-5 px-5 min-w-full flex flex-col">
+                <button type="submit" class="bg-black hover:bg-gray-800 text-white font-bold py-2 px-10 rounded">
                     Send Review
                 </button>
             </div>
@@ -124,6 +130,11 @@
 import { ref } from 'vue'
 import axios from 'axios'
 import { useUserStore } from "../../store/user";
+
+const randval1 = Math.floor(Math.random() * (Math.floor(20) - Math.ceil(1)) + Math.ceil(1))
+const randval2 = Math.floor(Math.random() * (Math.floor(20) - Math.ceil(1)) + Math.ceil(1))
+const actualval = randval1 + randval2
+const enterval = ref('')
 
 var userstore = useUserStore();
 const reviewtype = ref('')
@@ -139,66 +150,56 @@ const rating3 = ref('')
  * information to the database.
  */
 async function createreview() {
-    if (reviewtype.value === "course") {
-        await axios.post('http://localhost:3001/api/add/ratings/courses', {
-            user_id: userstore.user_id,
-            course: reviewselection.value,
-            prequisite_strictness: rating1.value,
-            pace: rating2.value,
-            depth: rating3.value
-        })
-        .then(function() {
-            console.log(reviewtype.value)
-            console.log(reviewselection.value)
-            console.log(rating1.value)
-            console.log(rating2.value)
-            console.log(rating3.value)
-            alert("Thank you for submitting your review!")
-        })
-        .catch(function(error) {
-            console.error();
-            alert(error);
-        })
-    } else if (reviewtype.value === "classroom") {
-        await axios.post('http://localhost:3001/api/add/ratings/classrooms', {
-            user_id: userstore.user_id,
-            classroom: reviewselection.value,
-            access_conv: rating1.value,
-            seating_quality: rating2.value,
-            technology_avail: rating3.value
-        })
-        .then(function() {
-            console.log(reviewtype.value)
-            console.log(reviewselection.value)
-            console.log(rating1.value)
-            console.log(rating2.value)
-            console.log(rating3.value)
-            alert("Thank you for submitting your review!")
-        })
-        .catch(function(error) {
-            console.error();
-            alert(error);
-        })
-    } else if (reviewtype.value === "ta") {
-        await axios.post('http://localhost:3001/api/add/ratings/tas', {
-            user_id: userstore.user_id,
-            ta: reviewselection.value,
-            question_answering: rating1.value,
-            grading_fairness: rating3.value,
-            responsiveness: rating2.value
-        })
-        .then(function() {
-            console.log(reviewtype.value)
-            console.log(reviewselection.value)
-            console.log(rating1.value)
-            console.log(rating2.value)
-            console.log(rating3.value)
-            alert("Thank you for submitting your review!")
-        })
-        .catch(function(error) {
-            console.error();
-            alert(error);
-        })
+    console.log(actualval, enterval.value)
+    if (actualval == enterval.value) {
+        if (reviewtype.value === "course") {
+            await axios.post('http://localhost:3001/api/add/ratings/courses', {
+                user_id: userstore.user_id,
+                course: reviewselection.value,
+                prequisite_strictness: rating1.value,
+                pace: rating2.value,
+                depth: rating3.value
+            })
+            .then(function() {
+                alert("Thank you for submitting your review!")
+            })
+            .catch(function(error) {
+                console.error();
+                alert(error);
+            })
+        } else if (reviewtype.value === "classroom") {
+            await axios.post('http://localhost:3001/api/add/ratings/classrooms', {
+                user_id: userstore.user_id,
+                classroom: reviewselection.value,
+                access_conv: rating1.value,
+                seating_quality: rating2.value,
+                technology_avail: rating3.value
+            })
+            .then(function() {
+                alert("Thank you for submitting your review!")
+            })
+            .catch(function(error) {
+                console.error();
+                alert(error);
+            })
+        } else if (reviewtype.value === "ta") {
+            await axios.post('http://localhost:3001/api/add/ratings/tas', {
+                user_id: userstore.user_id,
+                ta: reviewselection.value,
+                question_answering: rating1.value,
+                grading_fairness: rating3.value,
+                responsiveness: rating2.value
+            })
+            .then(function() {
+                alert("Thank you for submitting your review!")
+            })
+            .catch(function(error) {
+                console.error();
+                alert(error);
+            })
+        }
+    } else {
+        alert("CAPTCHA entered incorrectly. Please try again.")
     }
 }
 </script>
