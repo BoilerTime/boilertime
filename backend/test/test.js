2af36259682d06c2aaf783714eb934f16305115b
Onyx Mayer <101078769+hsmayer07@users.noreply.github.com>
2023-03-09 15:21:46 -0500

    Added Mocha Testing
    Installed the Mocha testing library and created a basic test condition to ensure that GET /api works as intended.

---
+++ backend/test/test.js
@@ -0,0 +1,29 @@
+// Import the dependencies for testing
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../index.js')
+// Configure chai
+chai.use(chaiHttp);
+chai.should();
+
+describe("GET API Index Page", () => {
+    // Test to get all students record
+    it("Index returns status 200", (done) => {
+        chai.request(app)
+            .get('/api')
+            .end((err, res) => {
+                res.should.have.status(200);
+                done();
+            });
+    });
+    // Test to get all students record
+    it("API Text Correct", (done) => {
+        chai.request(app)
+            .get('/api')
+            .end((err, res) => {
+                //res.should.have.status(200);
+                res.text.should.equal('API live!');
+                done();
+            });
+    });
+});
