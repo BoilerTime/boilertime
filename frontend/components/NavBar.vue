a6c1c9c7e05d1fce9ef04ab270eeb79ac23507f1
Zachary Will Sy <116471303+mapldx@users.noreply.github.com>
2023-03-12 17:29:32 -0400

    fix: profile route on nav bar

--- frontend/components/NavBar.vue
+++ frontend/components/NavBar.vue
@@ -8,7 +8,7 @@
         </a>
       </div>
       <div v-if="isLoggedIn" class="flex items-center justify-end">
-        <a href="/app/profile_page" class="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900 lg:mr-8">Your Profile</a>
+        <a href="/app/profile" class="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900 lg:mr-8">Your Profile</a>
         <a href="/app/create" class="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900 lg:mr-8">Create Schedule</a>
         Logged in as: {{ (userStore.user.user_id).slice(0,10) }}...
       </div>
