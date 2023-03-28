46907b74e73eea5ba2033cdcb63c0bda0ab0b186
mapldx <116471303+mapldx@users.noreply.github.com>
2023-03-28 19:06:45 -0400

    feat: guest mode log in

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ app.use(express.json());
 /* REMOVE ON PRODUCTION */
 /* REMOVE ON PRODUCTION */
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
