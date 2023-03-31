0b20e728a104fda065a661cd32beab3e6b0fd2da
Mahad Faruqi <mahadkhalid4955@gmail.com>
2023-03-31 00:01:17 -0400

    changed navbar from user_id to firstname lastname

--- frontend/components/NavBar.vue
+++ frontend/components/NavBar.vue
@@ -1,20 +1,20 @@
 
 <template>
   <header class="bg-white">
-    <nav class="mx-auto flex items-center justify-between py-8 px-12">
+    <nav class="flex items-center justify-between px-12 py-8 mx-auto">
       <div class="flex">
         <a href="/" class="-m-1.5 p-1.5">
-          <img class="h-10 w-auto" src="/logo.png" />
+          <img class="w-auto h-10" src="/logo.png" />
         </a>
       </div>
       <div v-if="isLoggedIn" class="flex items-center justify-end">
         <a href="/app/profile" class="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900 lg:mr-8">Your Profile</a>
         <a href="/app/create" class="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900 lg:mr-8">Create Schedule</a>
-        Logged in as: {{ (userStore.user.user_id).slice(0,10) }}...
+        Logged in as: {{ firstname + ' ' + lastname }}
       </div>
       <div v-else class="flex items-center justify-end">
         <a href="/auth/login" class="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900 lg:mr-8">Log in</a>
-        <a href="/auth/register" class="rounded-md bg-yellow-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:bg-yellow-600">Sign up</a>
+        <a href="/auth/register" class="px-3 py-2 text-sm font-semibold text-white bg-yellow-600 rounded-md shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:bg-yellow-600">Sign up</a>
       </div>
     </nav>
   </header>
@@ -32,24 +32,29 @@ var lastname = ref("")
 
 const userStore = useUserStore()
 
+const accessToken = userStore.accessToken;
+console.log('ACCESS TOKEN ' + accessToken);
+const config = {
+  headers: {
+    'authorization': `Bearer ${accessToken}`
+  }
+}
+
+
+
 try {
 } catch (err) {
   console.log(err)
 }
 
-isLoggedIn = userStore.isLoggedIn
 
-async function verifyToken() {
-  await userStore.verifyToken(userStore.accessToken, userStore.user_id).then((res) => {
-      //console.log(res.accessToken);
-  });
-}
+isLoggedIn = userStore.isLoggedIn
 
 async function getUserInfo() {
   axios
     .post("http://localhost:3001/api/get/profile/", {
       user_id: userStore.user_id,
-    })
+    }, config)
     .then((response) => {
       firstname.value = response.data.firstname;
       lastname.value = response.data.lastname;
@@ -61,7 +66,6 @@ async function getUserInfo() {
 
 onMounted(() => {
   getUserInfo();
-  verifyToken();
 });
 
 </script>
