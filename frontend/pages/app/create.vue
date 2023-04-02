cbb3ecb0c486492597ae89b7a8eb3e7678be52e8
Mahad Faruqi <mahadkhalid4955@gmail.com>
2023-04-02 12:27:50 -0400

    fixed not accessing isAGuest properly

--- frontend/pages/app/create.vue
+++ frontend/pages/app/create.vue
@@ -588,7 +588,7 @@ onMounted(async () => {
 })
 
 function submit() {
-  if (isAGuest) {
+   if (isAGuest.value) {
     toast.error("You must be logged in to use the optimizer!", {
       timeout: 5000,
       position: POSITION.TOP_CENTER
