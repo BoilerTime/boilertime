0bffb3ac5492b005a869abfb41ccd21a4efde0be
Mahad Faruqi <mahadkhalid4955@gmail.com>
2023-04-10 00:33:56 -0400

    more progress on purdue directory professors

--- backend/components/datasources/webscraper.py
+++ backend/components/datasources/webscraper.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 import pandas as pd
 import json
+import requests
+import re
 
 ##professors = []
 ##professors = pd.read_json('../../professors.json') 
@@ -22,7 +24,14 @@ driver.get(url)
 search_input = driver.find_element(By.ID, 'basicSearchInput')
 search_button = driver.find_element(By.ID, 'glass')
 
+f = open("professors_updated.json", "x")
+
+jArray = []
+
 for professor in professors:
+    if professor == 'Zheng, Wei':
+        break
+    json_object = {}
     print(professor)
     search_input = driver.find_element(By.ID, 'basicSearchInput')
     search_input.send_keys(professor)
@@ -34,71 +43,98 @@ for professor in professors:
 
     lists = driver.find_elements(By.CLASS_NAME, 'more')
     if (len(lists) > 1):
+        split = professor.split(',')
+        formatted_name = split[1].strip() + ' ' + split[0].strip()
+        try:
+            res = requests.get('https://api.purdue.io/odata/Instructors?$filter=contains(Name,%27' + formatted_name + '%27)')
+            response = json.loads(res.text)
+            email = response['value'][0]['Email']
+        except Exception as e:
+            json_object["name"] = professor
+            jArray.append(json_object)
+            continue
+
         for index in range(1, len(lists) + 1):
-            try:
-                name = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li[' + str(index) + ']/table/thead/tr/th/h2').text
-            except Exception as e:
-                name = "Name not found"
-            try:
-                alias = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li[' + str(index)+ ']/table/tbody/tr[1]/td').text
-            except Exception as e:
-                alias = "Alias not found"
-            try:
-                email = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li[' + str(index)+ ']/table/tbody/tr[2]/td/a').text
-            except Exception as e:
-                email = "Email not found"
-            try:
-                department = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li[' + str(index)+ ']/table/tbody/tr[4]/td').text
-            except Exception as e:
-                department = "Department not found"
-            try:
-                title = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li[' + str(index)+ ']/table/tbody/tr[5]/td').text
-            except Exception as e:
-                title = "Title not found"
+            should_continue = False
+            rows = driver.find_elements(By.TAG_NAME, 'tr');
+            for row in rows:
+                split = row.text.split(' ')
+                if split[0] == 'EMAIL':
+                    if split[1] == email:
+                        should_continue = True
+                        break
+                    else:
+                        should_continue = False
+            if should_continue:
+                for index, row in enumerate(rows):
+                    if (index == 0):
+                        json_object["name"] = professor
+                    else:
+                        match = re.match(r'([A-Z]+(?: [A-Z]+)?)\s(.+)', row.text)
+                        if match:
+                            key, value = match.groups()
+                            json_object[key.lower()] = value
+                jArray.append(json_object)
+                print(jArray)
+            elif not should_continue and index == len(lists):
+                json_object["name"] = professor
+                jArray.append(json_object)
+                print('this is index ' + str(index) + ' and this is the professor ' + professor)
+                print(jArray)
 
-            info = [name, alias, email, department, title]
-            print(info)
+
     elif(len(lists) > 0):
+        should_continue = False
+        split = professor.split(',')
+        formatted_name = split[1].strip() + ' ' + split[0].strip()
         try:
-            name = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li/table/thead/tr/th/h2').text
+            res = requests.get('https://api.purdue.io/odata/Instructors?$filter=contains(Name,%27' + formatted_name + '%27)')
+            response = json.loads(res.text)
+            email = response['value'][0]['Email']
+            should_continue = True
         except Exception as e:
-            name = "Name not found"
-            ##print(e)
+            should_continue = False
+        rows = driver.find_elements(By.TAG_NAME, 'tr');
+        if should_continue:
+            for row in rows:
+                split = row.text.split(' ')
+                if split[0] == 'EMAIL':
+                    if split[1] == email:
+                        should_continue = True
+                    else:
+                        should_continue = False
+            if should_continue:
+                for index, row in enumerate(rows):
+                    if (index == 0):
+                        json_object["name"] = professor 
+                    else:
+                        match = re.match(r'([A-Z]+(?: [A-Z]+)?)\s(.+)', row.text)
+                        if match:
+                            key, value = match.groups()
+                            json_object[key.lower()] = value
+                jArray.append(json_object)
+                print(jArray)
+            else:
+                json_object["name"] = professor 
+                jArray.append(json_object)
+                print(jArray)
+        else:
+            json_object["name"] = professor 
+            jArray.append(json_object)
+            print(jArray)
 
-        try:
-            alias = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li/table/tbody/tr[1]/td').text
-        except Exception as e:
-            alias = "Alias not found"
-            ##print(e)
-
-        try:
-            email = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li/table/tbody/tr[2]/td/a').text
-            //*[@id="results"]/ul/li/table/tbody/tr[3]/td/a
-        except Exception as e:
-            email = "Email not found"
-            ##print(e)
-
-        try:
-            department = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li/table/tbody/tr[4]/td').text
-        except Exception as e:
-            department = "Department not found"
-            ##print(e)
-
-        try:
-            title = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li/table/tbody/tr[5]/td').text
-        except Exception as e:
-            title = "Title not found"
-            ##print(e)
-
-        info = [name, alias, email, department, title]
-        print(info)
+    else:
+        json_object["name"] = professor 
+        jArray.append(json_object)
+        print(jArray)
 
     search_input = driver.find_element(By.ID, 'basicSearchInput')
     search_input.clear()
 
+json_string = json.dumps(jArray, ensure_ascii=False, separators=(',', ': '))
+f.write(json_string)
 driver.quit()
 
 
 ##soup = BeautifulSoup(page.content, 'html.parser')
-##print(page)
 
