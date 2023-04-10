ccc011f59739dd52ed98fca748116f85d2859505
Mahad Faruqi <mahadkhalid4955@gmail.com>
2023-04-09 20:14:48 -0400

    exception catching

--- backend/components/datasources/webscraper.py
+++ backend/components/datasources/webscraper.py
@@ -3,6 +3,15 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.common.by import By
+import pandas as pd
+import json
+
+##professors = []
+##professors = pd.read_json('../../professors.json') 
+
+with open('../../professors.json', 'r') as f:
+    professors = json.load(f)
 
 
 url = 'https://www.purdue.edu/directory/'
@@ -10,41 +19,82 @@ url = 'https://www.purdue.edu/directory/'
 driver = webdriver.Chrome(executable_path='./chromedriver')
 driver.get(url)
 
-search_input = driver.find_element_by_id('basicSearchInput')
-search_button = driver.find_element_by_id('glass')
-search_input.send_keys('PATRICIA M. WOLF')
-##search_input.send_keys('CHIRAYU GARG')
-search_button.click()
+search_input = driver.find_element(By.ID, 'basicSearchInput')
+search_button = driver.find_element(By.ID, 'glass')
 
-driver.implicitly_wait(10)
+for professor in professors:
+    print(professor)
+    search_input = driver.find_element(By.ID, 'basicSearchInput')
+    search_input.send_keys(professor)
+    ##search_input.send_keys('CHIRAYU GARG')
+    search_button = driver.find_element(By.ID, 'glass')
+    search_button.click()
 
-lists = driver.find_elements_by_class_name("more")
-print(len(lists))
-if (len(lists) > 1):
-    for index in range(1, len(lists) + 1):
-        name = driver.find_element_by_xpath('//*[@id="results"]/ul/li[' + str(index) + ']/table/thead/tr/th/h2').text
-        alias = driver.find_element_by_xpath('//*[@id="results"]/ul/li[' + str(index)+ ']/table/tbody/tr[1]/td').text
-        ##//*[@id="results"]/ul/li[1]/table/tbody/tr[1]/td
-        ##//*[@id="results"]/ul/li[2]/table/tbody/tr[1]/td
-        email = driver.find_element_by_xpath('//*[@id="results"]/ul/li[' + str(index)+ ']/table/tbody/tr[2]/td/a').text
-        department = driver.find_element_by_xpath('//*[@id="results"]/ul/li[' + str(index)+ ']/table/tbody/tr[4]/td').text
-        title = driver.find_element_by_xpath('//*[@id="results"]/ul/li[' + str(index)+ ']/table/tbody/tr[5]/td').text
-        ##school = driver.find_element_by_xpath('//*[@id="results"]/ul/li/table/tbody/tr[6]/td').text
-        info = [name, alias, email, department, title];
-        ##info = [name, alias, email, department, title, school];
-        print(info);
-else:
-    ##a = driver.find_elements_by_class_name("cn-name").get_attribute("textContent")
-    name = driver.find_element_by_xpath('//*[@id="results"]/ul/li/table/thead/tr/th/h2').text
-    alias = driver.find_element_by_xpath('//*[@id="results"]/ul/li/table/tbody/tr[1]/td').text
-    email = driver.find_element_by_xpath('//*[@id="results"]/ul/li/table/tbody/tr[2]/td/a').text
-    department = driver.find_element_by_xpath('//*[@id="results"]/ul/li/table/tbody/tr[4]/td').text
-    title = driver.find_element_by_xpath('//*[@id="results"]/ul/li/table/tbody/tr[5]/td').text
-    ##school = driver.find_element_by_xpath('//*[@id="results"]/ul/li/table/tbody/tr[6]/td').text
-    info = [name, alias, email, department, title];
-    ##info = [name, alias, email, department, title, school];
+    ##driver.implicitly_wait(10)
 
-    print(info);
+    lists = driver.find_elements(By.CLASS_NAME, 'more')
+    if (len(lists) > 1):
+        for index in range(1, len(lists) + 1):
+            try:
+                name = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li[' + str(index) + ']/table/thead/tr/th/h2').text
+            except Exception as e:
+                name = "Name not found"
+            try:
+                alias = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li[' + str(index)+ ']/table/tbody/tr[1]/td').text
+            except Exception as e:
+                alias = "Alias not found"
+            try:
+                email = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li[' + str(index)+ ']/table/tbody/tr[2]/td/a').text
+            except Exception as e:
+                email = "Email not found"
+            try:
+                department = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li[' + str(index)+ ']/table/tbody/tr[4]/td').text
+            except Exception as e:
+                department = "Department not found"
+            try:
+                title = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li[' + str(index)+ ']/table/tbody/tr[5]/td').text
+            except Exception as e:
+                title = "Title not found"
+
+            info = [name, alias, email, department, title]
+            print(info)
+    elif(len(lists) > 0):
+        try:
+            name = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li/table/thead/tr/th/h2').text
+        except Exception as e:
+            name = "Name not found"
+            ##print(e)
+
+        try:
+            alias = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li/table/tbody/tr[1]/td').text
+        except Exception as e:
+            alias = "Alias not found"
+            ##print(e)
+
+        try:
+            email = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li/table/tbody/tr[2]/td/a').text
+            //*[@id="results"]/ul/li/table/tbody/tr[3]/td/a
+        except Exception as e:
+            email = "Email not found"
+            ##print(e)
+
+        try:
+            department = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li/table/tbody/tr[4]/td').text
+        except Exception as e:
+            department = "Department not found"
+            ##print(e)
+
+        try:
+            title = driver.find_element(By.XPATH, '//*[@id="results"]/ul/li/table/tbody/tr[5]/td').text
+        except Exception as e:
+            title = "Title not found"
+            ##print(e)
+
+        info = [name, alias, email, department, title]
+        print(info)
+
+    search_input = driver.find_element(By.ID, 'basicSearchInput')
+    search_input.clear()
 
 driver.quit()
 
