3ad9376fd06c737dcbed157a233138a7a9a6f62a
Onyx Mayer <101078769+hsmayer07@users.noreply.github.com>
2023-04-15 16:24:01 -0400

    Implemented 2^n-arry tournament
    A new seeded tournament is now conducted following the set probabilities of success for the better individual.

--- optimizer/algorithm/Tournament.java
+++ optimizer/algorithm/Tournament.java
@@ -1,18 +1,41 @@
 package optimizer.algorithm;
+import optimizer.Utils;
 import optimizer.constants.Constants;
+import java.util.Random;
 
 public class Tournament {
     private final Schedule[] parentPool;    
-    private final int tournamentSize;
+    private final Random r; 
+    private final int tournamentSize; 
+    private final int prob;
 
-    public Tournament(Schedule[] parentPool, int tournamentSize) {
+    public Tournament(Schedule[] parentPool, Random r, int size, int prob) {
         this.parentPool = parentPool;
-        this.tournamentSize = tournamentSize;
+        this.r = r;
+        this.tournamentSize = size;
+        this.prob = prob;
     }
 
-    public Schedule[] tournament() {
-        return 
+    public Schedule tournament(Schedule[] target) {
+        return this.tournamentHelper(target);
     }
 
-    private Schedule[] tournamentHelper(Schedule )
+    private Schedule tournamentHelper(Schedule[] target) {
+        //Schedule[][] result = new Schedule[target.length - 1][target[0].length / 2];
+        if(target.length == 1) {
+            return target[0];
+        } else {
+            Schedule[] temp = new Schedule[target.length / 2];
+            for(int i = 0; i < temp.length; i++) {
+                Schedule x = target[i];
+                Schedule y = target[target.length - i];
+                if(Utils.randInRange(r, 1, 100) < prob) {
+                    temp[i] = x;
+                } else {
+                    temp[i] = y;
+                }
+            }
+            return tournamentHelper(temp);
+        }       
+    }
 }
