98a706dd52d42184ae80670eba3e258d24ae7737
NicholasPowers <35582171+Stale-Bread@users.noreply.github.com>
2023-04-16 19:46:37 -0400

    Added the save as PDF functionality
    Icons were added for saving as an image, PDF and links.
    Buttons were added for PDFs and links.
    Users can now export as a PDF.
    TODO: sharable link

--- frontend/pages/app/view/[term].vue
+++ frontend/pages/app/view/[term].vue
@@ -11,9 +11,22 @@
         <h1>Loading...</h1>
       </div>
       <div id="calendar" v-if="result.length > 0">
-          <button class="rounded-lg bg-yellow-500 hover:bg-yellow-700 px-4 py-2 text-sm font-bold border dark:border-black text-white" @click="screenie">
-            Screenie
-          </button>
+            <button class="rounded-lg bg-yellow-500 hover:bg-yellow-700 px-4 py-2 text-sm font-bold border dark:border-black text-white" @click="screenie">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
+              </svg>
+            </button>
+            <button class="rounded-lg bg-yellow-500 hover:bg-yellow-700 px-4 py-2 text-sm font-bold border dark:border-black text-white" @click="exportToPDF">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+              </svg>            
+            </button>
+            <button class="rounded-lg bg-yellow-500 hover:bg-yellow-700 px-4 py-2 text-sm font-bold border dark:border-black text-white" @click="copyLink">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+              </svg>                    
+            </button>
           <FullCalendar :options="calendarOptions" />
         </div>
         <div v-else class="h-screen p-12 bg-gray-200 dark:bg-neutral-500">
@@ -27,6 +40,7 @@
 import axios from 'axios';
 import { ref, onBeforeMount } from 'vue';
 import html2canvas from 'html2canvas';
+import html2pdf from "html2pdf.js";
 import { saveAs } from 'file-saver';
 import FullCalendar from '@fullcalendar/vue3'
 import timeGridPlugin from '@fullcalendar/timegrid'
@@ -90,6 +104,26 @@ async function screenie(){
   html2canvas(document.querySelector("#capture")).then(canvas => {
     saveAs(canvas.toDataURL(), 'schedule.png');
   });
+}
+
+/**
+ * This function is used to save a schedule in a PDF format
+ */
+async function exportToPDF() {
+  html2pdf(document.getElementById("capture"), {
+    margin: 0,
+    filename: "schedule.pdf",
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 2, dpi: 192, letterRendering: true },
+    jsPDF: { unit: "pt", format: 'A2', orientation: "landscape" }
+  });
+}
+
+/**
+ * This function will copy a link when the button is clicked
+ */
+async function copyLink() {
+  // TODO
 }
 
 let click = ''
