43d78f8484523bc11290a088ea44e9fe436e79d7
NicholasPowers <35582171+Stale-Bread@users.noreply.github.com>
2023-04-17 19:48:14 -0400

    Resolved text centering and double scroll bar

--- frontend/pages/group/view.vue
+++ frontend/pages/group/view.vue
@@ -1,15 +1,14 @@
 <!--view.vue is a page for viewing the groups that a user is in.-->
 <template>
     <NavBar />
-    <div class="h-screen p-16 bg-gray-200 overflow-auto dark:bg-neutral-500">
+    <div class="p-16 bg-gray-200 overflow-auto dark:bg-neutral-500">
         <!--Groups Page-->
-        <div
-            class="mx-auto max-w-6xl p-8 bg-white dark:bg-neutral-700 border border-black rounded-lg shadow-lg grid grid-flow-row">
+        <div class="mx-auto max-w-6xl p-8 bg-white dark:bg-neutral-700 border border-black rounded-lg shadow-lg grid grid-flow-row">
             <h1 class="font-bold text-2xl mb-5 text-center dark:text-gray-200">Groups Page</h1>
             <!--Flex grouping for groups-->
             <div class="bg-neutral-200 dark:bg-neutral-400 rounded-lg max-w-full mb-5 mt-5 p-4 text-center">
-                <ul class="list-inside list-item text-center">
-                    <li class="divide-y divide-solid divide-black text-center">
+                <ul class="list-inside list-item">
+                    <li class="divide-y divide-solid divide-black">
                         <li v-for="(item, index) in groups" :key="index">
                             <li class="mt-2 mb-2 font-bold">Group Name:</li>
                             <li class="mb-2 font-light">{{ item.group_name }}</li>
@@ -20,7 +19,7 @@
                             <li class="mb-2 font-bold">Invite Link:</li>
                             <li class="mb-2 font-light divide-y divide-dashed">{{ "localhost:3000/group/join/?group_id=" +
                                 item.group_id }}</li>
-                            <button type="copy" class="mb-2 w-1/8 bg-blue-500 hover:bg-blue-700 text-white font-bold border dark:border-black py-2 px-2 rounded-lg"
+                            <button type="copy" class="mb-2 w-1/8 bg-blue-500 hover:bg-blue-700 text-white font-bold border dark:border-black py-2 px-2 rounded-lg hover:"
                                 @click="copyLink(item.group_id)">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
