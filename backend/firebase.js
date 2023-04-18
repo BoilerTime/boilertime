acba01bba9d2563840e604d703aba129464618f9
mapldx <116471303+mapldx@users.noreply.github.com>
2023-04-18 01:51:04 -0400

    use hosted env var for firebase config

--- backend/firebase.js
+++ backend/firebase.js
@@ -2,5 +2,5 @@ const { initializeApp, applicationDefault, cert } = require('firebase-admin/app'
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
 initializeApp({
-  credential: cert('../config.json')
+  credential: cert('process.env.FIREBASE_SERVICE_ACCOUNT_KEY'),
 });
