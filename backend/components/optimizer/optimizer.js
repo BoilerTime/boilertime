fcab17e03c07c5ebf67d6be9b5d25a06a7f2c7da
Onyx Mayer <101078769+hsmayer07@users.noreply.github.com>
2023-04-20 15:38:13 -0400

    Primary Parsing

--- backend/components/optimizer/optimizer.js
+++ backend/components/optimizer/optimizer.js
@@ -29,96 +29,77 @@ async function optimizeSchedule(schedule) {
     }
 
     var output = [];
-    let resultFormat = {
+    const courseFormat = {
         "name": "", 
         "isRequired": false,
-        "startTimes": [], 
-        "durations": [], 
-        "daysOfWeek": [], 
-        "sectionIDs": [], 
-        "collectionIDs": [], 
-
-        "rmp": []
+        "sections": [],
+        "collections": []
     };
+
+
     
     for(let i = 0; i < optimizecourses.length; i++) {
-        //console.log(optimizecourses[i].split(" ")[0] + " + " + optimizecourses[i].split(" ")[1]);
-        let results = [];
-        let collectionResults = [];
-        await courses.collection(optimizecourses[i].split(" ")[0]).doc(optimizecourses[i].split(" ")[1]).listCollections().then((querySnapshot) => {
-            querySnapshot.forEach((collection) => {
-                results.push(collection.where("type", "in", ["Lecture", "Distance Learning"]).get());
-                collectionResults.push(collection.id);
-                //console.log(await collection.where("type", "==", "Lecture").get());
-                });
-            });
-        output.push(JSON.parse(JSON.stringify(resultFormat)))
-        output[i].name = optimizecourses[i];
-        output[i].isRequired = i < requiredLength;//optimizecourses[i].isRequired//schedule[i].isRequired
-        output[i].collectionIDs = collectionResults;
-        for(let j = 0; j < results.length; j++) {
-            results[j] = await results[j];
-            
-            results[j].forEach(async doc => {
-                let rawDur = doc.data().durations
-                let date = new Date(doc.data().starttime);
-                output[i].startTimes.push(date.getUTCHours()+ "" + date.getUTCMinutes());
-                output[i].durations.push(moment.duration(rawDur).hours()*60 + moment.duration(rawDur).minutes());
-                output[i].daysOfWeek.push(doc.data().daysOfWeek);
-                output[i].sectionIDs.push(doc.id);
-                
-                let name = doc.data().instructor.Name
-                ///console.log(await utils.getProfessorRating(name));
-                output[i].rmp.push((utils.getProfRatingsNoError(name)))
-              })
-        }
-    }
-
-    //await Promise.all(output).then((data) => {console.log(data)})
-    
-    for(let i = 0; i < output.length; i++) {
-        for(let j = 0; j < output[i].rmp.length; j++) {
-            let prom = output[i].rmp[j];
-            await prom.then((value) => {
-                output[i].rmp[j] = value.avgRating;
-            }).catch((error) => {
-                console.log(error);
-                output[i].rmp[j] = 0.0;
+      output.push(JSON.parse(JSON.stringify(courseFormat)));
+      //Make it easier to read stuff for now.
+      let currentCourse = output[i];
+      currentCourse.name = optimizecourses[i];
+      currentCourse.isRequired = i < requiredLength;
+      
+      await courses.collection(optimizecourses[i].split(" ")[0]).doc(optimizecourses[i].split(" ")[1]).listCollections().then((querySnapshot) => {
+          querySnapshot.forEach(async (collection) => {
+            let primaryInfoDB = await collection.where("type", "in", ["Lecture", "Distance Learning"]).get();
+            currentCourse.collections.push(collection.id);
+            primaryInfoDB.forEach(async doc => {
+              let data = doc.data();
+              let sectionFormat = {
+                "primary": {
+                  "startTime": "",
+                  "duration": "",
+                  "daysOfWeek": "",
+                  "rating": 0,
+                  "ID": "",
+                  "isFull": false,
+                  "type": ""
+                },
+                "secondary": []
+              }
+              let rawDur = data.durations
+              let date = new Date(data.starttime);
+              sectionFormat.primary.startTime = (date.getUTCHours()+ "" + date.getUTCMinutes());
+              sectionFormat.primary.duration = (moment.duration(rawDur).hours()*60 + moment.duration(rawDur).minutes());
+              sectionFormat.primary.daysOfWeek = data.daysOfWeek;
+              sectionFormat.primary.ID = (doc.id);
+              sectionFormat.primary.type = data.type
+              currentCourse.sections.push(sectionFormat);
+              
             })
-        }
-    }
+            
+            let secondaryInfoDB = await collection.where("type", "not-in", ["Lecture", "Distance Learning"]).get();
+            secondaryInfoDB.forEach(async doc => {
+              let data = doc.data();
+              const secondaryFormat = {
+                "startTime": "",
+                "duration": "",
+                "daysOfWeek": "",
+                "rating": 0,
+                "ID": "",
+                "isFull": false,
+                "type": ""
+              }
+              let rawDur = data.durations
+              let date = new Date(data.starttime);
+              sectionFormat.primary.startTime = (date.getUTCHours()+ "" + date.getUTCMinutes());
+              sectionFormat.primary.duration = (moment.duration(rawDur).hours()*60 + moment.duration(rawDur).minutes());
+              sectionFormat.primary.daysOfWeek = data.daysOfWeek;
+              sectionFormat.primary.ID = (doc.id);
+              sectionFormat.primary.type = data.type
+              currentCourse.sections.push(sectionFormat);
+              
+            })
 
-  collectionIDs = {};
-  jArray = [];
-  /*for (let i = 0; i < output.length; i++) {
-    isFull = await purdueio.isFull(output[i].name.split(' ')[0], output[i].name.split(' ')[1], output[i].sectionIDs);
-    console.log("UWU")
-    console.log(isFull);
-    output[i].isFull = isFull;
-    jArray = [];
-    for (let j = 0; j < output[i].collectionIDs.length; j++) {
-      sections = await scheduleSections.getSections(output[i].name.split(' ')[0], output[i].name.split(' ')[1], output[i].collectionIDs[j]); 
-      collectionIDs[output[i].collectionIDs[j]] = sections;
-      //console.log(collectionIDs);
-      jArray[output[i].collectionIDs[j]] = ({ collectionIDs })
-      //console.log(jArray[output[i].collectionIDs[j]]);
-      for (var k = 0; k < jArray[output[i].collectionIDs[j]].length; k++) {
-        //console.log('here');
-        //console.log('object ' + jArray[output[i].collectionIDs[j]][k]);
-      }
-      //console.log('jArray\n' + jArray)
-      //output[i].collectionIDs.remove(output[i].collectionIDs[j])
-      //output[i].collectionIDs.splice(j, 1);
+          });
+      })
     }
-    //delete output[i].collectionIDs;
-    //output[i].collectionIDs['collectionIDs'] = jArray;
-    output[i].collectionIDs = jArray;
-    if (i == 0) 
-      console.log('PROBELM \n\n' + JSON.stringify(output[i].collectionIDs['6de004cd-0840-43a6-907a-779d83ec5fd6']));
-    //console.log(jArray['collectionIDs']);
-
-    //console.log('\n\n' + jArray + '\n\n')
-  }*/
     console.log(output)
     return output;
 }
