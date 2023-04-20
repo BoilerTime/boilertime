acb7cf734a098fb39edbeb85d3c8e1ab2e73e10e
Joshua Yang <joshuajy03@gmail.com>
2023-04-20 18:33:03 -0400

    move blocker

--- frontend/pages/auth/login.vue
+++ frontend/pages/auth/login.vue
@@ -115,16 +115,7 @@ async function login() {
 }
 
 async function addCounterToFGPassword() {
-  console.log('here');
-  userStore.user.num_fg_clicks = userStore.num_fg_clicks + 1;
-  if (userStore.num_fg_clicks > 3) {
-    $toast.error("You have exceeded the amount of times you can reset passwrod! Please try again later.", {
-      timeout: 5000,
-    });
-  }
-  else {
-    navigateTo('/auth/forgotpassword');
-  }
+  navigateTo('/auth/forgotpassword');
 }
 
 async function guest() {
