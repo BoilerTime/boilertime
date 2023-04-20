6db2f6dc876a86b1b35377affafef2bbdee1e61f
Onyx Mayer <101078769+hsmayer07@users.noreply.github.com>
2023-04-20 13:42:10 -0400

    Change config.json

--- backend/firebase.js
+++ backend/firebase.js
@@ -2,5 +2,9 @@ const { initializeApp, applicationDefault, cert } = require('firebase-admin/app'
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
 initializeApp({
+<<<<<<< Updated upstream
   credential: cert('../config.json')
+=======
+  credential: cert('../config.json'),
+>>>>>>> Stashed changes
 });
